54d73456979b3b5f8b0582dd6708f7c1280139ce
ali-mahmoud24 <alimahmoud.2524@gmail.com>
2024-06-12 17:27:17 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+
+from CNN.model import show_info
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+
+UPLOAD_FOLDER = '/uploads'  # Define the upload folder path
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+@app.route('/')
+def hello():
+    return 'Hello, this is your Flask API!'
+
+
+# CNN
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return 'No file part'
+
+    file = request.files['image']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if file:
+      file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+      file.save(file_path)
+
+      name, info, questions = show_info(file_path)
+      return jsonify({'name': name, 'info': info, 'questions': questions})
+
+
+
+# # SENTIEMENT ANALYSIS
+
+# # Send data as json { "review": ['text..............'] }
+# @app.route('/predict', methods=['POST'])
+# def predict_review():
+
+#   review = request.json['review']
+
+#   rating = predict_sentimnet(review)
+
+#   return jsonify({'rating': rating})
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
